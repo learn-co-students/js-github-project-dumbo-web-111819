548a47c91e31ce4c86f44ae435977b9c6d537e87
ajshemi <akpojotor@yahoo.com>
2020-04-15 23:50:23 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,27 @@
 
-let saveInput=document.getElementById('search')
+document.addEventListener("DOMContentLoaded", function (){
 
+  const submitForm=document.querySelector('#github-form')
+  const userList=document.querySelector('#user-list')
+  // function handleSubmitForm(){
 
-function fetchData(){
-    let data = fetch(`https://api.github.com/search/users?q=${saveInput}`)
-      .then( res => res.json() )
-      .then( json => console.log(json) )
-}
+  // }
+  submitForm.addEventListener('submit', function(e){
+    e.preventDefault()
+    console.log(e.target['search'].value)
+    const newLi=document.createElement('li')
+  })
 
-fetchData()
+})
+
+// console.log(submitForm)
+
+// function fetchData(){
+//     let data = fetch(`https://api.github.com/search/users?q=${saveForm}`)
+//       .then( res => res.json() )
+//       .then( json => console.log(json) )
+// }
+
+// fetchData()
 
 // const button = document.getElementById("notify")
-saveInput.addEventListener('submit', function(){
-  console.log("Printing a Message!")
-})
